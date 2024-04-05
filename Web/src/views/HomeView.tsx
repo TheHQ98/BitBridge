569e2e86cc85bd6d5d53da4dd3646bab45f01fbd
Fan111i <yify@student.unimelb.edu.au>
2024-04-05 20:29:42 +1100

    merge the side bar with dateheader

--- Web/src/views/HomeView.tsx
+++ Web/src/views/HomeView.tsx
@@ -1,26 +1,17 @@
-import Timeline from '@mui/lab/Timeline';
-import TimelineItem from '@mui/lab/TimelineItem';
-import TimelineSeparator from '@mui/lab/TimelineSeparator';
-import TimelineConnector from '@mui/lab/TimelineConnector';
-import TimelineContent from '@mui/lab/TimelineContent';
-import TimelineDot from '@mui/lab/TimelineDot';
-import AddEvent from "../components/AddEvent.tsx";
-import {Container} from "@mui/material";
-import DateHeader from "../components/DateHeader.tsx";
-import DailyTaskChain from "../components/DailyTaskChain.tsx";
-import {useState} from "react";
+import React, { useState } from 'react';
+import { Container } from '@mui/material';
+import DateHeader from '../components/DateHeader.tsx';
+import DailyTaskChain from '../components/DailyTaskChain.tsx';
+import Header from '../components/Header.tsx';
 
 export default function HomeView() {
     const [selectedDate, setSelectedDate] = useState(new Date());
 
-
     return (
-        <>
-            <DateHeader selectedDate={selectedDate} onDateSelect={setSelectedDate}/>
+        <Container>
+            <Header />
+            <DateHeader selectedDate={selectedDate} onDateSelect={setSelectedDate} />
             <DailyTaskChain selectedDate={selectedDate} />
-            <AddEvent/>
-        </>
-
-
+        </Container>
     );
 }
